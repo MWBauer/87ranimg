2b522736ee67402b6f9c2ee4e5bd43ceba69165c
Marc <marc.jahservant@gmail.com>
2018-06-19 20:50:43 -0400

    Initial Commit of excercise 8.7

---
+++ Js/ranimg87.js
@@ -0,0 +1,17 @@
+var pictures = ["CPE", "EPT", "GPP", "GUI", "PERF", "PORT", "SEO"];
+//pick a random image from the pictures array and displays by crating an img tag and appending the src attributes to the filename
+document.write("<img src = \"" + "RanImg87/Images/fig08_07/" + pictures[Math.floor(Math.random() * 7)] + ".gif\" />");
+
+/*;const arr = [
+  "C:\Devfun\ServerTests\public\fig08_07\CPE.gif",
+  "C:\Devfun\ServerTests\public\fig08_07\EPT.gif",
+  "C:\Devfun\ServerTests\public\fig08_07\GPP.gif",
+  "C:\Devfun\ServerTests\public\fig08_07\GUI.gif",
+  "C:\Devfun\ServerTests\public\fig08_07\PERF.gif",
+  "C:\Devfun\ServerTests\public\fig08_07\PORT.gif",
+  "C:\Devfun\ServerTests\public\fig08_07\SEO.gif"
+];
+
+let oImg = document.createElement("img");
+oImg.setAttribute('src', arr[Math.floor(Math.random()*7)])0.;
+document.body.appendChild(oImg)*/
