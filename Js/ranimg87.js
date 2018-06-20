09f6a39bf156c25d0226d4b2bbb37f1e4cbe2fef
Marc <marc.jahservant@gmail.com>
2018-06-19 21:36:49 -0400

    File that works

--- Js/ranimg87.js
+++ Js/ranimg87.js
@@ -1,17 +1,17 @@
 var pictures = ["CPE", "EPT", "GPP", "GUI", "PERF", "PORT", "SEO"];
 //pick a random image from the pictures array and displays by crating an img tag and appending the src attributes to the filename
-document.write("<img src = \"" + "RanImg87/Images/fig08_07/" + pictures[Math.floor(Math.random() * 7)] + ".gif\" />");
+document.write("<img src=\"" + "Images/fig08_07/" + pictures[Math.floor(Math.random() * 7)] + ".gif\" />");
 
-/*;const arr = [
-  "C:\Devfun\ServerTests\public\fig08_07\CPE.gif",
-  "C:\Devfun\ServerTests\public\fig08_07\EPT.gif",
-  "C:\Devfun\ServerTests\public\fig08_07\GPP.gif",
-  "C:\Devfun\ServerTests\public\fig08_07\GUI.gif",
-  "C:\Devfun\ServerTests\public\fig08_07\PERF.gif",
-  "C:\Devfun\ServerTests\public\fig08_07\PORT.gif",
-  "C:\Devfun\ServerTests\public\fig08_07\SEO.gif"
+/*const arr = [
+  "Images\fig08_07\CPE.gif",
+  "Images\fig08_07\EPT.gif",
+  "Images\fig08_07\GPP.gif",
+  "Images\fig08_07\GUI.gif",
+  "Images\fig08_07\PERF.gif",
+  "Images\fig08_07\PORT.gif",
+  "Images\fig08_07\SEO.gif"
 ];
 
 let oImg = document.createElement("img");
-oImg.setAttribute('src', arr[Math.floor(Math.random()*7)])0.;
+oImg.setAttribute('src', arr[Math.floor(Math.random()*7)]);
 document.body.appendChild(oImg)*/
